5fcf6efb10f283fe23358535df300d09337571b5
LLD <lexi.latapi-dean@kpmg.co.uk>
2023-01-24 15:09:47 +0000

    initial review comments

--- src/BODSDataExtractor/example.py
+++ src/BODSDataExtractor/example.py
@@ -7,18 +7,26 @@
 # All that is needed is to enter your api key in the variable 'api', and run the code!
 # =============================================================================
 
-try:
-  from BODSDataExtractor.extractor import TimetableExtractor
-except:
-  from extractor import TimetableExtractor
+#try:
+from BODSDataExtractor.extractor import TimetableExtractor
+#except:
+#  from extractor import TimetableExtractor
   
-  
-api = 'enter api here'
+# LLD add a check to prevent difficult to trace errors - use isalnum() to check api is alphanumeric
+# and that it isn't equal to the original phrase. if it is, stop with error telling user
+# to ensure api key is set  
+api = 'c82016fb18cacbfafc4920b526bd72b3520f3ceb'
 
 #-------------------------------------------
 #            FINE TUNED RESULTS
 #-------------------------------------------
 #intiate an object instance called my_bus_data_object with desired parameters 
+
+# LLD the instantiator should not carry out the functionality itself.
+# separate this functionality to call the instantior and then
+# run (from this file) e.g. a get_timetables() method. Do not include analysis within
+# this method - that should be separate, to allow the user to choose which bits of
+# functionality they'd want to use and avoid unnecessary overhead.
 
 my_bus_data_object = TimetableExtractor(api_key=api
                                  ,status = 'published' 
@@ -28,6 +36,7 @@ my_bus_data_object = TimetableExtractor(api_key=api
                                  ,bods_compliant=True
                                  )
 
+# LLD dataset doesn't mean a lot - what will this mean in layman's terms?
 #save the extracted dataset level data to filtered_dataset_level variable
 filtered_dataset_level = my_bus_data_object.metadata
 
@@ -37,12 +46,18 @@ my_bus_data_object.save_metadata_to_csv()
 #save the extracted service line level data to dataset_level variable
 filtered_service_line_level = my_bus_data_object.service_line_extract
 
+# LLD method names should be as short as possible - e.g. service_lines_to_csv
 #save the extracted service line level data to lcoal csv file
 my_bus_data_object.save_service_line_extract_to_csv()
 
 #save the extracted stop level data to stop_level variable
 stop_level = my_bus_data_object.timetable_dict
 
+# LLD this should have the otpion to save as multiple csv or as one big csv.
+# equally it should be easy for user to combine the dictionary of dataframes into one df
+# (using whatever unique ID is used for files as the ID column in the df).
+# This will make it far easier for user to complete analysis over the whole
+# population
 #stop_level variable is a dictionary of dataframes, which can be saved to csv as follows (saves in downloads folder)
 my_bus_data_object.save_all_timetables_to_csv()
 
@@ -52,6 +67,11 @@ my_bus_data_object.save_all_timetables_to_csv()
 #-------------------------------------------
 #       REPORTING / ANALYTICS
 #-------------------------------------------
+
+# LLD I would suggest that all the below are combined into one function that prints out a df - 
+# run_DQ_report()) for example. I would not label this as analytics - really we don't need to include
+# analytics as the potential functionality could be infinite. Just need to make sure the data is supplied
+# in the best possible format for the user to begin their own analysis.
 count_of_operators = my_bus_data_object.count_operators() #returns count of distinct operators (measured by operator_name) in a chosen dataset
 
 count_of_service_codes = my_bus_data_object.count_service_codes()# returns count of unique service codes chosen dataset
--- src/BODSDataExtractor/otc_db_download.py
+++ src/BODSDataExtractor/otc_db_download.py
@@ -1,4 +1,12 @@
-#This file contains functions to download and save the database of the traffic commissioner to view registered bus services. Currently England only
+# This file contains functions to download and save the database of the traffic commissioner to view registered bus services. Currently England only
+# LLD please include standard header with version, update date, author etc. Brief overview of order of execution and explanation of
+# functions would also be useful.
+# please ensure the functions are written in a logical order, e.g. main function that calls others, then the other functions either
+# in the order they're called or most functional down to least functional ('helper' functions). To me it seems this
+# would best be refactored as 1) get_odc_db(save=True) ((see explanation below)),
+# then a much simpler save() function which combines the today/download folder/save code.
+
+
 import pandas as pd
 from datetime import date
 import requests
@@ -7,6 +15,9 @@ import os
 from pathlib import Path
 from sys import platform
 
+# LLD will these ever change? for readability please save a base URL (up to the word export) and then base url +
+# the csv part for each area
+
 west_england = 'https://content.mgmt.dvsacloud.uk/olcs.prod.dvsa.aws/data-gov-uk-export/Bus_RegisteredOnly_H.csv'
 west_midlands = 'https://content.mgmt.dvsacloud.uk/olcs.prod.dvsa.aws/data-gov-uk-export/Bus_RegisteredOnly_D.csv'
 london_south_east = 'https://content.mgmt.dvsacloud.uk/olcs.prod.dvsa.aws/data-gov-uk-export/Bus_RegisteredOnly_K.csv'
@@ -14,9 +25,12 @@ north_west_england = 'https://content.mgmt.dvsacloud.uk/olcs.prod.dvsa.aws/data-
 north_east_england = 'https://content.mgmt.dvsacloud.uk/olcs.prod.dvsa.aws/data-gov-uk-export/Bus_RegisteredOnly_B.csv'
 east_england = 'https://content.mgmt.dvsacloud.uk/olcs.prod.dvsa.aws/data-gov-uk-export/Bus_RegisteredOnly_F.csv'
 
-otc_db_files = [west_england, west_midlands, london_south_east, north_west_england, north_east_england, east_england]
+otc_db_files = [west_england, west_midlands, london_south_east,
+                north_west_england, north_east_england, east_england]
 
 today = str(date.today())
+
+# LLD consider allowing user to choose preferred folder for download, or downloading to user's current directory
 
 
 def get_user_downloads_folder():
@@ -33,6 +47,9 @@ def get_user_downloads_folder():
 
     return downloads_folder
 
+# LLD - doing extra logic. make the check for exists first, then if not create the folder.
+# use standard methods for checking if exists, see below link
+
 
 def create_today_folder():
     '''
@@ -40,9 +57,19 @@ def create_today_folder():
     '''
 
     today = str(date.today())
+
     downloads_folder = get_user_downloads_folder()
 
-    # list out the file names in the downloads folder
+# LLD this will result in an uncaught exception if downloads doesn't exist - you have provided a print message at Line 33 which
+# goes some way to helping the user, however it won't appear in the StackTrace.
+# Better approach: - get rid of the else block from line 32, - enclose the below in a try/catch (catch the
+# specific exception), - first, allow user to choose a download folder; - if still an exception, set the exception
+# error message to your message at Line 33
+
+# LLD - please replace the below with one of the following code options for best practice - user doesn't need a feedback message
+# https://www.tutorialspoint.com/How-can-I-create-a-directory-if-it-does-not-exist-using-Python. Combine steps for neater
+# code where possible
+    # list out the file names in the downloads folder - LLD this isn't needed - you would use path.exists(path) instead
     files = os.listdir(downloads_folder)
 
     # create the path for today folder in downloads
@@ -56,6 +83,13 @@ def create_today_folder():
         print('file with todays date already exists')
 
     return today_folder_path
+
+# LLD code blocks should never be duplicated. This function is
+# identical to fetch_otc_db() apart from the last few lines.
+# There are two options:
+# -call fetch_otc_db() within save_otc_db(), then save the result
+# - have one method with a default/optional argument for save option,
+#  e.g. get_otc_db(save=True)
 
 
 def save_otc_db():
@@ -90,9 +124,11 @@ def save_otc_db():
     create_today_folder()
     downloads_folder = get_user_downloads_folder()
 
+    # LLD presumably if the user executes multiple calls to the API with the extractor object,
+    # we don't need to run this every time - worth checking if this file path is already present (earlier on?)
     save_loc = downloads_folder + '/' + today + f'/otc_db_{today}.csv'
 
-    otc_db.to_csv(save_loc, index=False)   
+    otc_db.to_csv(save_loc, index=False)
     return otc_db
 
 
@@ -126,6 +162,7 @@ def fetch_otc_db():
     otc_db = otc_db.drop_duplicates()
 
     return otc_db
+
 
 if __name__ == "__main__":
     save_otc_db()
